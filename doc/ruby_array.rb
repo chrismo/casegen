15a827e7f61a0a0ff816399dc3635ee660c81225
chrismo <chrismo@clabs.org>
2021-02-27 16:56:40 -0600

    Add in output from original ruby_arry output.

--- doc/ruby_array.rb
+++ doc/ruby_array.rb
@@ -6,6 +6,17 @@ raise "This doesn't currently work. Do I even want to keep it?"
 
 # CLabs::CaseGen::CaseGen.new(DATA.read)
 
+# Outputs
+#
+# DataSubmitCase = Struct.new(:role, :authorization_code, :submit_enabled)
+#
+# cases = [DataSubmitCase.new("admin", "none", "true"),
+#          DataSubmitCase.new("admin", "invalid", "true"),
+#          DataSubmitCase.new("admin", "valid", "true"),
+#          DataSubmitCase.new("standard", "none", "false"),
+#          DataSubmitCase.new("standard", "invalid", "false"),
+#          DataSubmitCase.new("standard", "valid", "true")]
+
 __END__
 
 sets
